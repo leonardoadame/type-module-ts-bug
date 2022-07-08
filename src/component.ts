3fe1d23613c52aaafc3ce650e39caa3be44345ba
barak igal <baraki@wix.com>
2022-07-08 09:47:36 +0300

    Update component.ts

--- src/component.ts
+++ src/component.ts
@@ -1,4 +1,4 @@
-import * as T1 from "./component.st.css";
+import * as T1 from "./component.st.css"; // Cannot find module './component.st.css' or its corresponding type declarations.ts(2307)
 import * as T2 from "./component.st.css.js";
 
 T1.classes.root; // any
